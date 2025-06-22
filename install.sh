6c9e7f006099307bdba85a79440a157e8eeeee57
pglearning <jeff1617327238@outlook.com>
2025-06-22 15:38:53 +0800

    1.03

--- install.sh
+++ install.sh
@@ -21,6 +21,9 @@ archmirrors_country="CN"
 
 # Only support ext4 and btrfs, default=ext4
 filesystem="btrfs"
+# Only support systemd-boot and grub, default=systemd-boot
+bootloader="grubs"
+# ISSUE: systemd-boot not support btrfs?
 
 # "" disable, Disk must formated befor disable
 format_disk="/dev/sda"
@@ -99,7 +102,7 @@ if [ "$WIPE_DISK" == "n" -o "$WIPE_DISK" == "N" ]; then exit 4; fi
 # Main Install
 default_install() {
 
-    # Network Test
+    # Network Test, may need iwctl
     if ping -c 1 -W 1 "$test_address"; then
         echo -e "$OK ping success!"
     else
@@ -108,15 +111,14 @@ default_install() {
     fi
     
     # Date & Time
-    echo -e "$INFO Sync date & time..."
     timedatectl set-ntp true
     timedatectl set-timezone $timezone
+    echo -e "$OK Sync date & time."
     
     # reflector will add some shit mirrors in the file
     systemctl stop reflector.service
     if [ -n "$mirrors" ]; then
         # Custom Pacman Mirrorlist
-        echo -e "$INFO mirrorlist overwrite..."
         echo "##" > /etc/pacman.d/mirrorlist
         echo "## Arch Linux mirrorlist" >> /etc/pacman.d/mirrorlist
         echo "## Generated on $(date '+%Y-%m-%d %H:%M:%S')" >> /etc/pacman.d/mirrorlist
@@ -129,6 +131,7 @@ default_install() {
         curl -L "https://archlinux.org/mirrorlist/?country=${archmirrors_country}&protocol=https" -o /etc/pacman.d/mirrorlist
         sed -i 's/#Server/Server/' /etc/pacman.d/mirrorlist
     fi
+    echo -e "$OK mirrorlist overwrite."
 
     # Keyring update (live pacman download issue)
     pacman -Sy archlinux-keyring --noconfirm
@@ -136,7 +139,6 @@ default_install() {
 
     # Format Disk (gpt + uefi)
     if [ "$format_disk" != "" ]; then
-        echo -e "$INFO Format disk ${format_disk}."
         ## Create GUID Partition Table(GPT), will wipe all data from disk
         # echo $WIPE_DISK | parted $format_disk mklabel gpt     # (Danger) use first confirm auto format disk
         parted $format_disk mklabel gpt
@@ -152,10 +154,10 @@ default_install() {
             parted $format_disk mkpart $root_label ext4 "$(($efi_size+$swap_size))$unit" 100%
         fi
         parted $format_disk type 3 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
+        echo -e "$OK Format disk ${format_disk}."
     fi
     
     # Format Partition
-    echo -e "$INFO Format partition..."
     sleep 1
     mkfs.fat -F32 $efi_partition
     mkswap $swap_partition
@@ -163,7 +165,6 @@ default_install() {
     # compress= "zstd": forcing compression; "lzo" fast raw compression; autodefrag
     if [ "$filesystem" == "btrfs" ]; then
         ## btrfs, live need package: btrfs-progs
-        # pacman -S btrfs-progs --noconfirm --needed
         mkfs.btrfs -fL $btrfs_label $root_partition
         mount --mkdir -t btrfs -o compress=lzo,autodefrag $root_partition /mnt
         btrfs subvolume create /mnt/@
@@ -192,55 +193,102 @@ default_install() {
     mkdir /mnt/etc
     genfstab -U /mnt > /mnt/etc/fstab
     cat /mnt/etc/fstab
+    echo -e "$OK Format partition."
 
     # Install Base packages
-    echo -e "$INFO Base packages install..."
     pacstrap -K /mnt ${packages[@]} --noconfirm --needed
+    echo -e "$OK Basic packages install."
 
     # Setup System 
     ## NOTE: "arch-chroot /mnt echo string > file" not working
     ## NOTE: "arch-chroot /mnt <<EOF" or "<<-EOF" output all line, cant use if []; then
     ## Timezone & Generate /etc/adjtime
-    arch-chroot /mnt echo -e "$INFO Setting timezone..."
     arch-chroot /mnt ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
     arch-chroot /mnt hwclock --systohc
+    echo -e "$OK Setup timezone."
     ## Language
-    arch-chroot /mnt echo -e "$INFO Setting system language..."
     arch-chroot /mnt sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
     arch-chroot /mnt sed -i 's/#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen
     arch-chroot /mnt sed -i 's/#zh_TW.UTF-8 UTF-8/zh_TW.UTF-8 UTF-8/' /etc/locale.gen
     arch-chroot /mnt locale-gen
-    arch-chroot /mnt sh -c "echo 'LANG=en_US.UTF-8' > /etc/locale.conf"   # may not working
+    arch-chroot /mnt sh -c "echo 'LANG=en_US.UTF-8' > /etc/locale.conf"
+    echo -e "$OK Setup system language."
     ## Hostname
-    arch-chroot /mnt echo -e "$INFO Setting hostname..."
-    arch-chroot /mnt sh -c "echo $hostname > /etc/hostname"   # may not working
+    arch-chroot /mnt sh -c "echo $hostname > /etc/hostname"
+    echo -e "$OK Setup hostname."
     ## Passwd root
     while true; do
         arch-chroot /mnt echo -e "$INPUT Enter root password: "
-        if arch-chroot /mnt passwd root; then break; fi
+        if arch-chroot /mnt passwd root; then
+            echo -e "$OK Setup root password."
+            break
+        fi
     done
-    ## Group wheel execute any command
-    arch-chroot /mnt echo -e "$INFO Setting sudoers..."
-    arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-    
-    ## Gurb
-    arch-chroot /mnt echo -e "$INFO Setting gurb..."
-    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$grub_label
-    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-
-    # (opt)Custom Setting
-    ## Enable pacman color
-    arch-chroot /mnt echo -e "$INFO Enable pacman color..."
-    arch-chroot /mnt sed -i 's/#Color/Color/' /etc/pacman.conf
-
     ## Create first user
     if [ "$username" != "" ]; then
         arch-chroot /mnt useradd -m -G wheel -s $usershell $username
+        echo -e "$OK Setup $username (group wheel)."
         while true; do
             arch-chroot /mnt echo -e "$INPUT Enter $username password: "
-            if arch-chroot /mnt passwd $username; then break; fi
+            if arch-chroot /mnt passwd $username; then
+                echo -e "$OK Setup $username password."
+                break
+            fi
         done
     fi
+
+    ## Group wheel execute any command
+    arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+    echo -e "$OK Setup group wheel sudoers."
+    
+    if [ "$bootloader" = "grub" ]; then
+        ## Grub
+        arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$grub_label
+        arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+        echo -e "$OK Setup grub."
+    else
+        ## systemd-boot
+        arch-chroot /mnt bootctl --esp-path=/boot --boot-path=/boot install     # auto find esp-path (/efi /boot/efi /boot) boot-path (/boot)
+        ## systemd-boot-pacman-hook
+        if ! [ -d "/mnt/etc/pacman.d/" ]; then mkdir /mnt/etc/pacman.d; fi
+        if ! [ -d "/mnt/etc/pacman.d/hooks/" ]; then mkdir /mnt/etc/pacman.d/hooks; fi
+        arch-chroot /mnt sh -c "echo '[Trigger]' > /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'Type = Package' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'Operation = Upgrade' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'Target = systemd' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo '' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo '[Action]' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'Description = Gracefully upgrading systemd-boot...' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'When = PostTransaction' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt sh -c "echo 'Exec = /usr/bin/systemctl restart systemd-boot-update.service' >> /etc/pacman.d/hooks/95-systemd-boot.hook"
+        arch-chroot /mnt bootctl update
+        ### loader menu, console-mode can be set by auto, keep (hardware resolution)
+        echo 'default  arch.conf' > /mnt/boot/loader/loader.conf
+        echo 'timeout  3' >> /mnt/boot/loader/loader.conf
+        echo 'console-mode auto' >> /mnt/boot/loader/loader.conf
+        echo 'editor   1' >> /mnt/boot/loader/loader.conf
+        ### loadconf
+        ROOT_UUID=$(blkid -s UUID -o value $root_partition)
+        root_flags=""
+        if [ "$filesystem" == "btrfs" ]; then
+            root_flags="rootflags=subvol=@"
+        fi
+        arch-chroot /mnt sh -c "echo 'title   Arch Linux' > /boot/loader/entries/arch.conf"
+        arch-chroot /mnt sh -c "echo 'linux   /vmlinuz-linux' >> /boot/loader/entries/arch.conf"
+        arch-chroot /mnt sh -c "echo 'initrd  /initramfs-linux.img' >> /boot/loader/entries/arch.conf"
+        arch-chroot /mnt sh -c "echo 'options root=UUID=$ROOT_UUID rw $root_flags' >> /boot/loader/entries/arch.conf"
+
+        arch-chroot /mnt sh -c "echo 'title   Arch Linux (fallback initramfs)' > /boot/loader/entries/arch-fallback.conf"
+        arch-chroot /mnt sh -c "echo 'linux   /vmlinuz-linux' >> /boot/loader/entries/arch-fallback.conf"
+        arch-chroot /mnt sh -c "echo 'initrd  /initramfs-linux-fallback.img' >> /boot/loader/entries/arch-fallback.conf"
+        arch-chroot /mnt sh -c "echo 'options root=UUID=$ROOT_UUID rw $root_flags' >> /boot/loader/entries/arch-fallback.conf"
+        echo -e "$OK Setup systemd-boot."
+    fi
+
+    # (opt)Custom Setting
+    ## Enable pacman color
+    arch-chroot /mnt sed -i 's/#Color/Color/' /etc/pacman.conf
+    echo -e "$OK Enable pacman color."
 
     # Custom command
     if [ -d "/mnt/home/$username" ]; then
@@ -254,10 +302,10 @@ default_install() {
             script_path="${script_path%/*}"
         fi
         cp -r $script_path/myconfig /mnt/home/$username/
+        echo -e "$OK Copy to /home/$username/myconfig finsh!"
     fi
 
-    echo -e "$OK Setup finsh!"
-
+    echo -e "$OK All setup finsh!"
     # Reboot now
     printf "$INPUT Reboot now? [Y/n] "
     read -re reboot_now
